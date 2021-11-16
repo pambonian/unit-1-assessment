dc6d54d4cb715e3be11335be0c6357c357a75cb2
pambonian <pambonian@gmail.com>
2021-11-16 11:44:25 -0500

    deliverable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
 document.addEventListener("DOMContentLoaded", function(){
-    //....
+    let input = document.getElementById('input');
+    input.setAttribute("value", "1");
 });
+
+let addButton = document.getElementById('add');
+let subtractButton = document.getElementById('subtract');
+let output = document.getElementById('output');
+let input = document.getElementById('input');
+
+function add () {
+    let outputValue = parseInt(output.textContent);
+    let inputValue = parseInt(input.value);
+    outputValue = outputValue + inputValue;
+    output.textContent = outputValue;
+    if (outputValue < 0) {
+        output.style.color = 'red';
+    } else {
+        output.style.color = 'black';
+    }
+    
+}
+
+function subtract () {
+    let outputValue = parseInt(output.textContent);
+    let inputValue = parseInt(input.value);
+    outputValue = outputValue - inputValue;
+    output.textContent = outputValue;
+    if (outputValue < 0) {
+        output.style.color = 'red';
+    } else {
+        output.style.color = 'black';
+    }
+}
+
+function interaction () {
+    addButton.addEventListener('click', function() {
+        add();
+    })
+    subtractButton.addEventListener('click', function() {
+        subtract();
+    })
+    addButton.addEventListener('mouseover', function() {
+        addButton.style.color = "grey";
+        addButton.style.background = "white";
+    })
+    addButton.addEventListener('mouseleave', function() {
+        addButton.style.color = "white";
+        addButton.style.background = "grey";
+    })
+    subtractButton.addEventListener('mouseover', function() {
+        subtractButton.style.color = "grey";
+        subtractButton.style.color = "white";
+    })
+    subtractButton.addEventListener('mouseleave', function() {
+        subtractButton.style.color = "white";
+        subtractButton.style.color = "grey";
+    })
+}
+
+interaction();
